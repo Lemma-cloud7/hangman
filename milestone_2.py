c43e0cc75fb6c21499bd8651b691d87abcb7bd78
Teodora Nechifor <panayotova.teodora29@gmail.com>
2023-10-27 09:40:51 +0100

    Adding extra miles to the fisrt milestone for Hangman

--- milestone_2.py
+++ milestone_2.py
@@ -1,7 +1,29 @@
-import random
+#import random
 
+#word_list = ["apple", "banana", "kiwi", "grapes", "melon"]
+
+#word = random.choice(word_list)
+#print(word)
+
+#guess = input("Enter a single letter: ")
+
+#if len(guess)==1 and guess.isalpha():
+	#print("Good guess!")
+#else:
+	#print("Oops! That is not a valid input."
+import random
 
 word_list = ["apple", "banana", "kiwi", "grapes", "melon"]
 
 word = random.choice(word_list)
 print(word)
+
+guess = input("Enter a single letter: ")
+
+if len(guess)==1 and guess.isalpha():
+    if guess in word:
+    	print("Good guess!")
+    else:
+        print("Oops! That is not a valid input.")
+
+
