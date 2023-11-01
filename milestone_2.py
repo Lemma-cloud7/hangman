dcc28ae046afa8e17ab26379115e0e4a39b570e0
Teodora Nechifor <panayotova.teodora29@gmail.com>
2023-11-01 10:01:00 +0000

    Add milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -10,7 +10,7 @@
 #if len(guess)==1 and guess.isalpha():
 	#print("Good guess!")
 #else:
-	#print("Oops! That is not a valid input."
+	#print("Oops! That is not a valid input.")
 import random
 
 word_list = ["apple", "banana", "kiwi", "grapes", "melon"]
@@ -23,7 +23,7 @@ guess = input("Enter a single letter: ")
 if len(guess)==1 and guess.isalpha():
     if guess in word:
     	print("Good guess!")
-    else:
-        print("Oops! That is not a valid input.")
+else:
+    print("Oops! That is not a valid input.")
 
 
